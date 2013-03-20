7c99bf9b5b5c247d3c2a8e89d743681115ac5022
Peteris Krumins <peteris.krumins@gmail.com>
2013-03-20 09:28:12 +0200

    almost works

---
+++ index.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+var spawn = require("child_process").spawn;
+var temp = require('temp');
+
+module.exports = function (opts, cb) {
+    fs.readFile(opts.template, {encoding: 'utf8'}, function (err, template) {
+        template = template.toString()
+            .replace('%from%', opts.from)
+            .replace('%to%', opts.to)
+            .replace('%period%', opts.period)
+            .replace('%amount%', opts.amount.replace("$", "\\$"));
+
+        temp.open('invoice', function (err, info) {
+            if (err) {
+                cb("Error creating a temporary file: " + err);
+                return;
+            }
+            fs.write(info.fd, Buffer(template), 0, template.length, null, function (err) {
+                if (err) {
+                    cb("Error writing invoice template to a temporary file: " + err);
+                    return;
+                }
+                fs.close(info.fd, function (err) {
+                    if (err) {
+                        cb("Error closing invoice template's fd: " + err);
+                        return;
+                    }
+                    var latex = spawn("pdflatex", ["-interaction=nonstopmode", "-halt-on-error", info.path]);
+                    function onExit (code) {
+                        if (code) {
+                            cb("Spawning latex failed. Error code: " + code);
+                            return;
+                        }
+                        cb(null, info.path, info.path.replace('.tex', '.pdf'));
+                    }
+                    latex.on('exit', onExit);
+                });
+            });
+        });
+    });
+}
