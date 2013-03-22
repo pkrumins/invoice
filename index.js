767eca50ce1fe03e00790cd12fb3fe9dd6f2da88
Peteris Krumins <peteris.krumins@gmail.com>
2013-03-22 04:47:34 +0200

    chdir to os.tmpDir before generating the invoice. return pdf invoice in the final callback

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 var fs = require('fs');
 var spawn = require("child_process").spawn;
 var temp = require('temp');
+var chdir = require('chdir');
+var os = require('os');
 
 module.exports = function (opts, cb) {
-    fs.readFile(opts.template, {encoding: 'utf8'}, function (err, template) {
-        template = template.toString()
-            .replace('%from%', opts.from)
-            .replace('%to%', opts.to)
-            .replace('%period%', opts.period)
-            .replace('%amount%', opts.amount.replace("$", "\\$"));
+    chdir(os.tmpDir(), function () { 
+        fs.readFile(opts.template, {encoding: 'utf8'}, function (err, template) {
+            template = template.toString()
+                .replace('%from%', opts.from)
+                .replace('%to%', opts.to)
+                .replace('%period%', opts.period)
+                .replace('%amount%', opts.amount.replace("$", "\\$"));
 
-        temp.open('invoice', function (err, info) {
-            if (err) {
-                cb("Error creating a temporary file: " + err);
-                return;
-            }
-            fs.write(info.fd, Buffer(template), 0, template.length, null, function (err) {
+            temp.open('invoice', function (err, info) {
                 if (err) {
-                    cb("Error writing invoice template to a temporary file: " + err);
+                    cb("Error creating a temporary file: " + err);
                     return;
                 }
-                fs.close(info.fd, function (err) {
+                fs.write(info.fd, Buffer(template), 0, template.length, null, function (err) {
                     if (err) {
-                        cb("Error closing invoice template's fd: " + err);
+                        cb("Error writing invoice template to a temporary file: " + err);
                         return;
                     }
-                    var latex = spawn("pdflatex", ["-interaction=nonstopmode", "-halt-on-error", info.path]);
-                    function onExit (code) {
-                        if (code) {
-                            cb("Spawning latex failed. Error code: " + code);
+                    fs.close(info.fd, function (err) {
+                        if (err) {
+                            cb("Error closing invoice template's fd: " + err);
                             return;
                         }
-                        cb(null, info.path, info.path.replace('.tex', '.pdf'));
-                    }
-                    latex.on('exit', onExit);
+                        var latex = spawn("pdflatex", ["-interaction=nonstopmode", "-halt-on-error", info.path]);
+                        function onExit (code) {
+                            if (code) {
+                                cb("Spawning latex failed. Error code: " + code);
+                                return;
+                            }
+                            cb(null, info.path + '.pdf');
+                        }
+                        latex.on('exit', onExit);
+                    });
                 });
             });
         });
